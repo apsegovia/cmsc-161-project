781410c51270b20eb23ab29435e60a49b86b402f
apsegovia <apsegovia@up.edu.ph>
2026-05-24 01:47:04 +0800

    add; mesh js object

---
+++ mesh.js
@@ -0,0 +1,62 @@
+export default class Mesh {
+
+    constructor(gl, name, data) {
+        this.gl = gl;
+        this.name = name;
+
+        // just to verify
+        this.vertices = data.vertices;
+        this.normals = data.normals;
+        this.uvs = data.uvs;
+        this.indices = data.indices;
+
+        // buffers
+        this.vertexBuffer = null;
+        this.normalBuffer = null;
+        this.uvBuffer = null;
+        this.indexBuffer = null;
+
+        // for transformation
+        this.TM = this.identity();
+
+        this.buildBuffers();
+    }
+
+    identity() {
+        return [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ];
+    }
+
+    buildBuffers() {
+        const gl = this.gl;
+
+        // Vertex Buffer
+        this.vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+
+        // Normal Buffer
+        this.normalBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.normals), gl.STATIC_DRAW);
+
+        // UV Buffer
+        this.uvBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uvBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.uvs), gl.STATIC_DRAW);
+
+        // 4. Index Buffer
+        this.indexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(this.indices), gl.STATIC_DRAW);
+
+    }
+
+    draw(program) {
+        // bind + draw here
+    }
+}
